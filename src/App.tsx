dd125915ed74f27360fcc388aa30e7e571cb486b
Rai Medeiros <raismedeiros@gmail.com>
2020-09-07 20:01:57 -0300

    Autenticaçao de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
-      {/* <SignUp /> */}
+      <AuthProvider>
+        <SignIn />
+        {/* <SignUp /> */}
+      </AuthProvider>
       <GlobalStyle />
     </>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -9,30 +9,47 @@ import LogoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
+import { AuthContext } from '../../context/AuthContext';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const { user, signIn } = useContext(AuthContext);
 
-      // schema de validação
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail é de preenchimento obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('A senha deve ser preenchida'),
-      });
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      const errors = getValidationErrors(err);
+  console.log(user);
 
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        // schema de validação
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail é de preenchimento obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('A senha deve ser preenchida'),
+        });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
 
   return (
     <Container>
